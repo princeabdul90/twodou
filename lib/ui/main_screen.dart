0f2e1aca296ff27f55c4a7e73f1742b25a7c6da6
princeabdul90 <abuabdu99@gmail.com>
2022-08-01 19:45:43 +0800

    first commit

---
+++ lib/ui/main_screen.dart
@@ -0,0 +1,14 @@
+/*
+* Developer: Abubakar Abdullahi
+* Date: 01/08/2022
+*/
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.green,);
+  }
+}
