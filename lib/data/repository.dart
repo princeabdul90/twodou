069ca76bb1ada5301de67c00ebc4b5cfbb9cad0d
princeabdul90 <abuabdu99@gmail.com>
2022-08-07 08:22:01 +0800

    task crud functionality added

--- lib/data/repository.dart
+++ lib/data/repository.dart
@@ -3,19 +3,35 @@
 * Date: 03/08/2022
 */
 
-import 'model/category.dart';
+import 'model/model.dart';
 
 abstract class Repository {
+
+  //*** CATEGORY METHODS  *****
   // Find Methods
   Future<List<Category>> findAllCategories();
   Stream<List<Category>> watchAllCategories();
   Future<Category> findCategoryById(int id);
+
   // Update Methods
   Future<int> updateCategoryTaskCount(Category category);
   // Delete Methods
   Future<void> deleteCategory(Category category);
   // Insert Method
   Future<int> insertCategory(Category category);
+
+  //*** TASK METHODS  *****
+  // Find Methods
+  Future<List<Task>> findAllTasks();
+  Stream<List<Task>> watchAllTasks();
+  Future<Task> findTaskById(int id);
+
+  // update
+  Future<int> updateTask(Task task);
+  // insert
+  Future<int> insertTask(Task task);
+  // delete
+  Future<void> deleteTask(Task task);
 
 
 
